25959cb258fc8d529e51ca12dc9d3157e8041bed
cazador481 <cazador481@users.noreply.github.com>
2024-05-24 17:24:42 -0700

    fish updates

---
+++ dot_config/private_fish/conf.d/_nvidia.fish
@@ -0,0 +1,52 @@
+# Set XDG directories, also set in ~/.cshrc_custom
+set -xg XDG_DATA_HOME /home/eash/scratch/.local
+set -xg XDG_CACHE_HOME /home/eash/scratch/.cache
+
+# P4 Env variables
+set -xg P4CONFIG .p4config
+set -xg P4IGNORE .p4ignore:/home/eash/.p4ignore
+
+set -xg CC gcc
+
+# set -xg -p LM_LICENSE_FILE /home/tools/admin/license_files
+
+# LSF Setup
+set -xg LSF_SERVERDIR /usr/local/lsf/etc
+
+fish_add_path --path --prepend --move \
+    /home/nv/utils/client_tool/4.2.1/bin \
+    /home/utils/fzf-0.29.0/bin \
+    /home/autosubmit/bin \
+    /home/nv/utils/crucible/1.0/bin \
+    /home/nv/utils/hwmeth/bin \
+    /home/nv/utils/quasar/bin \
+    /home/utils/Python/3.9/3.9.9-20220110/bin/ \
+    /home/utils/direnv-2.30.3/bin \
+    /home/utils/gcc-11.2.0/bin \
+    /home/utils/git-2.42.0/bin \
+    /home/utils/go-1.18/bin/ \
+    /home/utils/htop-1.0/bin/ \
+    /home/utils/jq-1.6/bin \
+    /home/utils/llvm-14.0.0/bin \
+    /home/utils/make-4.3/bin \
+    /home/utils/neovim-0.8.0/bin \
+    # /home/utils/node-v14.5.0/bin \
+    /home/utils/node-v16.16.0/bin \
+    /home/utils/ruby-3.2.2/bin \
+    /home/utils/rust-1.71.0/bin/ \
+    /home/utils/shellcheck-v0.7.1/ \
+    /home/utils/tmux-3.2a/bin \
+    /usr/local/lsf/bin \
+    /home/utils/fd-8.1.1/bin \
+    /home/utils/ripgrep-13.0.0/bin \
+    /home/nv/bin
+fish_add_path --path --append -- move /home/utils/bin /bin
+    # /home/utils/the_silver_searcher-0.32.0/bin
+    # /home/utils/mosh-1.3.2-90-g03087e7/bin
+
+if test -f /home/nv/utils/ci/nvci_setup/nvci-setup.sh
+    bass source /home/nv/utils/ci/nvci_setup/nvci-setup.sh
+end
+
+#  alias nvim="CC=gcc CXX=g++ LDFLAGS='-static-libgcc -static-libstdc++' command nvim" */
+alias nvim="CC=clang CXX=clang++ LDFLAGS='-static-libgcc -static-libstdc++' command nvim"
