3d5295a9f8bd3b957f4a53a75fc1543baea7c060
cazador481 <cazador481@users.noreply.github.com>
2024-06-06 21:39:58 -0400

    up to date lazy

--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -7,49 +7,46 @@ local severities = {
     note = vim.diagnostic.severity.HINT,
 }
 return {
-    "mfussenegger/nvim-lint",
-    opts = {
-        linters_by_ft = {
-            fish = { "fish" },
-            python = { "mypy" },
-            sh = { "shellcheck" },
-        },
-        -- LazyVim extension to easily override linter options
-        -- or add custom linters.
-        ---@type table<string,table|function>
-        linters = {
-            mypy = function()
-                local command = function()
-                    local return_code = os.execute("poetry run mypy --version 1>/dev/null 2>/dev/null")
-                    if return_code == 0 then
-                        return { "poetry", "run", "mypy" }
-                    end
-                    return "mypy"
-                end
-                return {
-                    stdin = false,
-                    ignore_exitcode = true,
-                    # must be a path
-                    -- cmd = command(),
-                    cmd = ".venv/bin/mypy",
-                    args = {
-                        "--show-column-numbers",
-                        "--show-error-end",
-                        --                    '--hide-error-codes',
-                        "--hide-error-context",
-                        "--no-color-output",
-                        "--no-error-summary",
-                        "--no-pretty",
-                    },
-                    parser = require("lint.parser").from_pattern(
-                        pattern,
-                        groups,
-                        severities,
-                        { ["source"] = "mypy" },
-                        { end_col_offset = 0 }
-                    ),
-                }
-            end,
-        },
-    },
+	"mfussenegger/nvim-lint",
+	opts = {
+		linters_by_ft = {
+			fish = { "fish" },
+			python = { "mypy" },
+			sh = { "shellcheck" },
+		},
+		-- LazyVim extension to easily override linter options
+		-- or add custom linters.
+		---@type table<string,table|function>
+		linters = {
+			mypy = function()
+				local command = function()
+					if vim.fn.execute(".venv/bin/mypy") == 1 then
+						return ".venv/bin/mypy"
+					end
+					return "mypy"
+				end
+				return {
+					stdint = false,
+					ignore_exitcode = true,
+					cmd = command(),
+					args = {
+						"--show-column-numbers",
+						"--show-error-end",
+						--                    '--hide-error-codes',
+						"--hide-error-context",
+						"--no-color-output",
+						"--no-error-summary",
+						"--no-pretty",
+					},
+					parser = require("lint.parser").from_pattern(
+						pattern,
+						groups,
+						severities,
+						{ ["source"] = "mypy" },
+						{ end_col_offset = 0 }
+					),
+				}
+			end,
+		},
+	},
 }
