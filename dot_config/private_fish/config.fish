7ff50950f1cb0b7a9ccfa53b4a78e08ccea93ec1
cazador481 <cazador481@users.noreply.github.com>
2024-06-06 21:27:28 -0400

    fix typo

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,8 @@
 # Note this runs after conf.d/*
+if status is-interactive
+    and not set -q TMUX
+    tmux attach ;or tmux
+end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
