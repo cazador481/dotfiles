bc2487c9325ef32e0d52723d5f7eb999d037c15f
cazador481 <cazador481@users.noreply.github.com>
2024-06-11 08:05:21 -0700

    disable pyright at work, and enable pylsp instead

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,3 +1,4 @@
+local lsputil = require('lspconfig/util')
 return {
     "neovim/nvim-lspconfig",
     init = function()
@@ -16,6 +17,7 @@ return {
                 },
             },
             pyright = {
+                enabled = not vim.g.using_nvidia_farm,
                 settings = {
                     python = {
                         pythonPath =".venv/bin/python",
@@ -40,7 +42,50 @@ return {
                     indexing = true,
                 },
             },
+            pylsp = {
+                enabled = vim.g.using_nvidia_farm,
+                -- cmd = { "/home/eash/.local/share/nvim/mason/bin/pylsp" },
+                -- cmd = { '/home/eash/scratch/.local/nvim/lsp_servers/pylsp/venv/bin/pylsp', '-v', '-v', '--log-file', '/home/eash/pylsp.log' },
+                cmd_env = {
+                    VIRTUAL_ENV = ".venv",
+                    PATH = lsputil.path.join(".venv", "bin") .. ":" .. vim.env.PATH,
+                },
+                settings = {
+                    pylsp = {
+                        configurationSources = { "flake8" },
+                        plugins = {
+                            black = { enabled = false },
+                            -- flake8 = {
+                            --     enabled = false,
+                            --     ignore = { "BLK100", "E1", "E2", "E3", "E5", "I", "W291" },
+                            --     executable = ".venv/bin/flake8",
+                            -- },
+                            jedi = { environment = ".venv/bin/python" },
+                            mccabe = { enabled = false },
+                            mypy = { enabled = false }, -- mypy does not use executable
+                            pycodestyle = { enabled = false },
+                            pydocstyle = { enabled = false },
+                            pyflakes = { enabled = false },
+                            pylint = { enabled = false },
+
+                            yapf = { enabled = false },
+                            ruff = {enabled = false},
+
+                            --  Enable auto import dd
+                            jedi_completion = { enabled = false},
+                            -- rope_autoimport = { enabled = true },
+                            rope_completion = {enabled = true},
+                        },
+                    },
+                },
+                on_new_config = function(new_config, new_root_dir)
+                    local py = require("utils.python.lua")
+                    py.env(new_root_dir)
+                    new_config.settings.pylsp.plugins.jedi.environment = py.get_python_dir(new_root_dir)
+                end,
+            },
         },
+
      setup = {
     ruff_lsp = function()
       LazyVim.lsp.on_attach(function(client, _)
